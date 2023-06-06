efbe822888f09ee5ee284f09806922a72a6afaf8
Glenn Edgar <glenn-edgar@onynxengr.com>
2023-06-05 17:12:17 -0700

    integerating

--- CFL_c/test_c/macros/events.lua
+++ CFL_c/test_c/macros/events.lua
@@ -45,3 +45,59 @@ function Send_event_queue(queue_table,event_data_ptr)
    
 end
 
+function Reset_event_queues(queue_table)
+    print("made it here")
+    local array_name = queue_table.array_name
+    local queue_number = queue_table.queue_number
+    message = string.format("     Asm_reset_named_event_queues_CFL(input,%s,%s);\n",queue_number,array_name)
+    file:write(message)
+end
+
+
+function Prepare_rpc_message()
+    -- TBD
+end
+
+function Send_rpc_server(server_queue)
+    -- TBD
+end
+
+function Wait_rpc_message(queue)
+
+    -- TBD
+end
+
+function Dispose_rpc_message()
+
+    -- TBD
+end
+
+function Wait_rpc_message(queue)
+
+    -- TBD
+end 
+
+function Wait_rpc_response_message(queue)
+
+    -- TBD
+end
+
+function  Send_rpc_response_message()
+
+    -- TBD
+end
+
+function Generate_event_queues(queue_name,queue_list)
+    
+    local return_value = {}
+    return_value.queue_number = #queue_list
+
+    local queue_arrary  = table.concat(queue_list,",")
+  
+    local message = string.format("    const char *%s[] = {%s};\n",queue_name,queue_arrary)
+   
+    file:write(message)
+    return_value.array_name = queue_name
+    
+    return return_value
+end
